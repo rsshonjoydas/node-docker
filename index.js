2b4ce9193ca2e9c9735a6751f687eb6df8f443c0
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-04 18:11:27 +0600

    Docker networks & Communication with DNS

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// ! Database connection
+const uri = "mongodb://shonjoy:rsshonjoy@mongo:27017/?authSource=admin"
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+mongoose
+  .connect(uri, options)
+  .then(() => console.log('Successfully connected to DB'))
+  .catch((e) => {
+    console.log(e)
+  })
 
 app.use(express.json())
 
