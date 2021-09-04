a0f08ef470fb69e894caaeb27587d1f5c3371a77
rsshonjoydas <rsshonjoydas@gmail.com>
2021-09-05 02:35:07 +0600

    user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel")
 const bcrypt = require("bcryptjs")
 
+// ! User sign up
 exports.signup = async (req, res) => {
   const { username, password } = req.body;
 
@@ -19,6 +20,39 @@ exports.signup = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: "fail"
+    })
+  }
+}
+
+// ! User login
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username })
+
+    if (!user) {
+      return res.status(404).json({
+        status: 'fail',
+        message: 'User authentication failed!'
+      })
+    }
+
+    const isValidPwd = await bcrypt.compare(password, user.password)
+
+    if (isValidPwd) {
+      res.status(200).json({
+        status: "Successful"
+      })
+    } else {
+      res.status(400).json({
+        status: 'failed',
+        message: 'Incorrect username or password!'
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      status: "failed"
     })
   }
 }
